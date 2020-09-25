a63c2d097a25c76eb6101080eb5cf63e1a067437
Gabriel Sierra <gsierra@groupenordik.com>
2020-09-25 18:27:51 -0400

    * navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { Link } from 'gatsby'
 import Header from '../components/header'
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div style={{color: `purple`}}>
-    <Link to="/contact/">Contact</Link>
-    <Header />
-    <p>what a world.</p>
+  return <Layout> <div>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+      </p>
     <img src="https//source.unsplash.com/random/400x2000" alt="" />
-  </div>
+  </div></Layout>
 }
