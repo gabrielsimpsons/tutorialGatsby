1380d930d104b1e0301289696a4793ebd930f8fa
Gabriel Sierra <gsierra@groupenordik.com>
2020-09-26 13:53:47 -0400

    * read and transform markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
